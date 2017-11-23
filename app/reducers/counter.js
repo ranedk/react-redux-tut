94187d6599fa1ce0c66202374bb618788e5df130
Devendra K. Rane <ranedk@gmail.com>
2017-11-23 18:59:53 +0530

    Lets make things immutable
    Using Immutability helps us do the right thing in react (refer README)
    So now, we should always define states with ImmutabilityJS

--- app/reducers/counter.js
+++ app/reducers/counter.js
@@ -10,4 +10,17 @@ const counter = (state = 0, action) => {
   }
 }
 
+export const addCounter = (list) => {
+  // This is bad, changing state is BAD! WTF?!
+  // list.push(0); 
+  // return list;
+
+  // See what I did here
+  // change bhi kar diya aur pata bhi nahin chala
+  // change the object in a way that a new object
+  // is created and returned.
+  // ImmutableJS helps us do these things, we'll see
+  return list.concat([0])
+}
+
 export default counter
--- test/reducers/counter.spec.js
+++ test/reducers/counter.spec.js
@@ -1,7 +1,20 @@
 import counter from 'reducers/counter'
+import { addCounter } from 'reducers/counter'
+import { List } from 'immutable'
 
 describe('counter', () => {
     it("should increment counter", () => {
         expect(counter(undefined, {type: 'INCREMENT'})).toBe(1);
+    })
+})
+
+
+describe('addCounter', () => {
+    const listBefore = List([]);
+    const listAfter = List([0]);
+
+    it("should add a counter", () => {
+        expect(addCounter(listBefore))
+        .toEqual(listAfter);
     })
 })
